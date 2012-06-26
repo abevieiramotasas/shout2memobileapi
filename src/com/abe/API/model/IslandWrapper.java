c331ee206db3ba774a60dd0db7910fec889a8ce3
Abelardo <abbbekgb@gmail.com>
2012-06-26 17:13:04 -0300

    adicionado wrapper da gambiarra heheheheheh

--- src/com/abe/API/model/IslandWrapper.java
+++ src/com/abe/API/model/IslandWrapper.java
@@ -3,14 +3,30 @@ package com.abe.API.model;
 import java.util.List;
 
 public class IslandWrapper {
-	private List<IslandIn> islands;
+	private WrapperGambiarra islands;
+
+	private IslandWrapper() {
+		this.islands = new WrapperGambiarra();
+	}
 
 	public void setIslands(List<IslandIn> islands) {
-		this.islands = islands;
+		this.islands.setIslands(islands);
 	}
 
 	public List<IslandIn> getIslands() {
-		return islands;
+		return islands.getIslands();
+	}
+
+	private class WrapperGambiarra {
+		private List<IslandIn> islands;
+
+		public void setIslands(List<IslandIn> islands) {
+			this.islands = islands;
+		}
+
+		public List<IslandIn> getIslands() {
+			return islands;
+		}
 	}
 
 }
